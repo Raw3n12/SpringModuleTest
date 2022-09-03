e603d406574ade160937f1df909917b59a4ed379
Raw3n12 <aron.peccol@gmail.com>
2022-09-03 12:35:41 +0200

    1st commit 1&2 feladat

---
+++ src/main/java/app/repositories/CrewRepo.java
@@ -0,0 +1,8 @@
+package app.repositories;
+
+import app.models.Crew;
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface CrewRepo extends CrudRepository<Crew, Long> {
+}
